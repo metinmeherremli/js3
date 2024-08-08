e8df6eefb6e92004683149fb0fa9e8576a541ffa
metinmherremli <metinmeherremli99@gmail.com>
2024-08-08 12:37:16 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+ var text = document.getElementById("fist")
+ console.log(text)
+ text.innerText ="first"
+ var text2 = document.getElementById("second")
+ console.log(text2)
+ text2.innerText ="second"
+ var text3 = document.getElementById("third")
+ console.log(text3)
+ text3.innerText ="third"
+ var text4 = document.getElementById("fourth")
+ console.log(text4)
+ text4.innerText="fourth"
+ var text5 =document.getElementById("fifth")
+ console.log(text5)
+ text5.innerText="fifth"
+
